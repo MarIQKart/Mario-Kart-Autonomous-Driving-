731dcae36492d9a4487ea0dcae8d9e5320d824f9
MarIQKart <70724245+MarIQKart@users.noreply.github.com>
2020-11-22 23:41:24 -0700

    Clean up
    Cleaned up the file and added better functionality for emulating key presses

--- EmulatorInterface.py
+++ EmulatorInterface.py
@@ -1,4 +1,5 @@
-import keyboard # keyboard library for sending native WindowsOS key-presses to the emulators
+import time
+import keyboard
 
 class EmulatorInterface:
 
@@ -8,9 +9,9 @@ class EmulatorInterface:
             of the emulator that you're going to be simulating
             the key presses for. It is not case sensitive
     '''
-    def __init__(self, emulator):
+    def __init__(self, emulator, game):
         emulator = emulator.lower() # Converts the string to all lower case
-
+        game = game.lower() # Converts the string to all lower case
         '''
             This is the input mapping dictionary. The first
             parameter is the name of the emulator you're using.
@@ -42,15 +43,17 @@ class EmulatorInterface:
             {
                 "mupen 64":
                 {
-                    "throttle": "x",
-                    "left": "left",
-                    "right": "right",
-                    "up": "up",
-                    "down": "down"
-                },
+                    "mario kart":
+                    {
+                        "throttle": 'x',
+                        "left": 'l',
+                        "right": 'r',
+                        "up": 'u',
+                        "down": 'd'
+                    }
+                }
             }
-        self.currentEmulatorMapping = self.inputMapping[emulator] # Here we keep track of the emulator inputs we need
-
+        self.currentEmulatorMapping = self.inputMapping[emulator][game] # Here we keep track of the emulator inputs we need for a game
 
     '''
         :desc:
@@ -65,10 +68,90 @@ class EmulatorInterface:
     def emulatePress(self, input):
         if input in self.currentEmulatorMapping: # Checks to see if the input mapping exists
             inputMapping = self.currentEmulatorMapping[input] # Grabs the mapping
-            keyboard.press(inputMapping) # Presses the key corresponding to the mapping
+            self.holdKey(inputMapping, 0.15)
         else: # If there was no input mapping
             print("Not a valid input mapping") # Alert the user before quitting
             exit(-1) # Quits the program
+
+
+    '''
+        :desc:
+            This function is used to hold a key
+            for a certain amount of time. This is
+            used because the emulator will have a certain amount
+            of time that a key must be pressed for in order
+            for the event to be registered by it.
+            
+        :param key:
+            This is the key we want to have pressed down
+            
+        :param holdTime:
+            This is the parameter that controls how
+            long to hold the key down (the units are in seconds).
+            
+            For example, half a second: holdTime = 0.5
+    '''
+    def holdKey(self, key, holdTime):
+        start = time.time() # gets the current time
+        while (time.time() - start) < holdTime: # As long as the change in time is less than the holdTime
+            keyboard.press(key) # Then keep pressing the key
+
+    '''
+        :desc:
+            This function takes in an array, maps the inputs,
+            and then presses all the keys in the array.
+            
+        :param inputs:
+            This is an array of the inputs that can be entered
+            to the function.
+            
+            Example:
+                inputs = ["throttle", 'left', 'up']
+                
+                The emulator will press them in that order
+    '''
+    def emulatePresses(self, inputs):
+        mappedInputs = [] # An array of the mapped inputs
+        for input in inputs: # Loops through the inputs
+            if input not in self.currentEmulatorMapping: # Checks to see if the input mapping exists
+                print("Not a valid input mapping for: {}".format(input))
+                exit(-1) # If it isn't a valid mapping, then fail
+            inputMapping = self.currentEmulatorMapping[input] # Grabs the mapping
+            mappedInputs.append(inputMapping) # Adds the valid mapping to the list
+
+        '''
+            Loops through the current mapped inputs and holds
+            them down for 0.15 seconds (the minimum amount of time that
+            the emulator will need in order for a key to be registered.)
+        '''
+        for key in mappedInputs:
+            self.holdKey(key, 0.15) # Holds the current key
+
+    '''
+        :desc:
+            This function takes in an array, maps the inputs,
+            and then releases all the keys in the array.
+
+        :param inputs:
+            This is an array of the inputs that can be entered
+            to the function.
+
+            Example:
+                inputs = ["throttle", 'left', 'up']
+
+                The emulator will release them in that order
+    '''
+    def emulateReleasePresses(self, inputs):
+        mappedInputs = []
+        for input in inputs:
+            if input not in self.currentEmulatorMapping:  # Checks to see if the input mapping exists
+                print("Not a valid input mapping for: {}".format(input))
+                exit(-1)
+            inputMapping = self.currentEmulatorMapping[input]  # Grabs the mapping
+            mappedInputs.append(inputMapping)
+
+        for key in mappedInputs:
+            keyboard.release(key)
 
     '''
         :desc:
@@ -87,7 +170,7 @@ class EmulatorInterface:
             key presses (and their corresponding mappings)
             that the emulator currently has.
     '''
-    def getPossibleEmulations(self, mappings = False):
+    def getPossibleKeyEmulations(self, mappings = False):
         if mappings: # If the user wants the mappings then return them
             return self.currentEmulatorMapping
         else: # Else, just give us the list of values the emulator can accept as inputs for presses
